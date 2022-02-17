424fbd370e3cc56f1a2f7b2117a68487931bed92
Odden69 <helena.joh2@gmail.com>
2022-02-17 19:52:59 +0000

    ttt

---
+++ taskmanager/static/js/scritp.js
@@ -0,0 +1,6 @@
+/* If you use jquery get the other snippet of code */
+document.addEventListener('DOMContentLoaded', function() {
+    // sidenav initialization
+    let sidenav = document.querySelectorAll('.sidenav');
+    M.Sidenav.init(sidenav);
+  });
